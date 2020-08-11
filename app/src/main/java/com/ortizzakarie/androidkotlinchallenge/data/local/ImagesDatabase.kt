6de42d1402f6048c3f225d3310987b2b5f174a05
Zakarie Ortiz <ZOrtiz42@outlook.com>
2020-08-10 22:18:50 -0400

    ADD
    - Added the ImagesDatabase class to store images for offline capabilities.
    
    - Added the ImagesDatabaseModule for dependency injection and being able to provide the Database and the ImageDao to the repository.

---
+++ app/src/main/java/com/ortizzakarie/androidkotlinchallenge/data/local/ImagesDatabase.kt
@@ -0,0 +1,44 @@
+package com.ortizzakarie.androidkotlinchallenge.data.local
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.ortizzakarie.androidkotlinchallenge.data.local.dao.ImageDao
+import com.ortizzakarie.androidkotlinchallenge.model.Image
+
+@Database(
+    entities = [Image::class],
+    version = 1
+)
+abstract class ImagesDatabase : RoomDatabase() {
+
+    abstract fun getImageDao(): ImageDao
+
+    companion object {
+        const val DB_NAME = "image_database"
+
+        @Volatile
+        private var INSTANCE: ImagesDatabase? = null
+
+        fun getInstance(context: Context): ImagesDatabase{
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    ImagesDatabase::class.java,
+                    DB_NAME
+                ).build()
+
+                INSTANCE = instance
+                return instance
+            }
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/ortizzakarie/androidkotlinchallenge/di/module/ImagesDatabaseModule.kt
@@ -0,0 +1,25 @@
+package com.ortizzakarie.androidkotlinchallenge.di.module
+
+import android.app.Application
+import com.ortizzakarie.androidkotlinchallenge.data.local.ImagesDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import javax.inject.Singleton
+
+@InstallIn(ApplicationComponent::class)
+@Module
+class ImagesDatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabase(application: Application) = ImagesDatabase.getInstance(application)
+
+    @Singleton
+    @Provides
+    fun provideImageDao(database: ImagesDatabase) = database.getImageDao()
+
+
+
+}
